84919000e4d7ef7dbfed65526d4c7f4f319a4c2d
Yeasin Ahmed <61079600+YsnAhmd@users.noreply.github.com>
2020-02-26 16:14:12 +0600

    Add files via upload

---
+++ forloopex1.py
@@ -0,0 +1,15 @@
+numbers=[1,2,3,4,5,6,]
+for x in numbers:
+    print(x)
+
+"""
+for val in sequence:
+    body of for
+"""
+
+for x in range(4,13):
+    print(x)
+
+for x in range(15):
+    if x%2 !=0:
+        print(x)
---
+++ forloopex2.py
@@ -0,0 +1,7 @@
+for x in [5,8,1,4]:
+    print(x,end='\t')
+
+
+name=['Tonoya','Anni','Nikita','Buri']
+for x in name:
+    print(x,end='\t')
